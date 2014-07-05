3cb25e667354521be69ecf7a92e4cd809c8ed996
Leon Cheung <me@leoncheung.com>
2014-07-05 01:20:16 -0700

    Testing out angular

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
-window.onload = function(e) {
-	console.log("hello");
-}
+(function () {
+	var module = angular.module("ironBlog", []);
+	module.controller("BlogController", ["$scope", function($scope) {
+		$scope.header = "robot revitalized with tape";
+		$scope.author = "bobby joe";
+		$scope.date = "oh god date parsing"
+		$scope.text = "test text";
+		console.log($scope.text);
+	}]);
+})();
